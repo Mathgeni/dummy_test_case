fcf712079f7aa34cdb8dd8537cef744b55ee2a89
mathgeni <kanunnikov44@gmail.com>
2023-09-27 20:54:11 +0300

    compose added servers are ok

--- dummy_client/client.py
+++ dummy_client/client.py
@@ -8,20 +8,20 @@ import aiohttp
 
 async def client(urls: list[str]) -> list[dict[str, str | int | datetime]]:
     customers = [
-        'Youson Clark',
-        'Zink Wilson',
-        'Yoder Johnson',
-        'Zeis Lewis',
-        'Yakley Robinson',
-        'Yates Ross',
-        'Yorston Olsen',
-        'Zbikowski Robinson',
-        'Yeomans Haugen',
-        'Zillgitt Pedersen',
+        'Abigail',
+        'Alexandra',
+        'Alison',
+        'Amanda',
+        'Amelia',
+        'Amy',
+        'Andrea',
+        'Angela',
+        'Anna',
+        'Anne'
     ]
     responses = list()
     async with aiohttp.ClientSession() as session:
-        for _ in range(100):
+        for _ in range(20):
             data = {
                 'name': random.choice(customers),
                 'text': ''.join(random.choice(string.ascii_lowercase) for _ in range(15))
@@ -33,9 +33,14 @@ async def client(urls: list[str]) -> list[dict[str, str | int | datetime]]:
 
 
 async def main():
-    urls = ['http://127.0.0.1:8000/']
-    tasks = [asyncio.create_task(client(urls)) for _ in range(50)]
+    urls = ['http://127.0.0.1:9000/', 'http://127.0.0.1:9001/']
+    tasks = [asyncio.create_task(client(urls)) for _ in range(10)]
     result = await asyncio.gather(*tasks)
+    for item in result:
+        for messanger in item:
+            for message in messanger:
+                print(message)
+            print()
 
 
 if __name__ == '__main__':
